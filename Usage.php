0aba39e6d36999ba64f2371716c29b735c32a418
Maxim Levchenko <maximl1st@gmail.com>
2016-03-25 05:11:04 +0200

    composer.json

--- Usage.php
+++ Usage.php
@@ -3,7 +3,8 @@
  * @package maximalist\GetImages
  */
 
-require_once( 'Harvest.php' );
+// require_once( 'Harvest.php' );
+require 'vendor/autoload.php';
 
 $h = new maximalist\GetImages\Harvest( 'http://umj.com.ua', '/home/maxim/Work/Test/img', 2 );
 $h->make();
